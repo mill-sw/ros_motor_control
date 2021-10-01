0b9a45308d7d902669ab0598a6ebe7fd79213a4a
mill-sw <87070457+mill-sw@users.noreply.github.com>
2021-10-01 18:08:58 +0900

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -2,28 +2,32 @@ import rospy
 from std_msgs.msg import UInt16MultiArray
 
 
-pub = rospy.Publisher('cmd_vel', UInt16MultiArray, queue_size=10)
-cmd_vel = UInt16MultiArray()
-cmd_vel.data = [0, 0]
+pub = rospy.Publisher('controller', UInt16MultiArray, queue_size=10)
+data_output = UInt16MultiArray()
+data_output.data = [0, 0]
 
 
-def callback1(msg):
+def callback(msg):
     rospy.loginfo(msg.data[0])
-    cmd_vel.data[0] = msg.data[0]
-    cmd_vel.data[1] = msg.data[1]
-    
-    pub.publish(cmd_vel)
+    data_output.data[0] = msg.data[0]
+    data_output.data[1] = msg.data[1]
+
+    pub.publish(data_output)
 
 
 def motor_control():
     rospy.init_node('motor_control', anonymous=True)
-    rospy.Subscriber("roscar_teleop_cmd_vel", UInt16MultiArray, callback1)
-    pub.publish(cmd_vel)
+    rospy.Subscriber("roscar_teleop_data_output", UInt16MultiArray, callback)
+    pub.publish(data_output)
     rospy.spin()
 
 
-if __name__ == '__main__':
+def main():
     try:
         motor_control()
     except rospy.ROSInterruptException:
         pass
+
+
+if __name__ == '__main__':
+    main()
